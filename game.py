d99aa670ca4b257314057f394a7b9b14de42d4fb
kos17u844 <jeni62@mail.ru>
2021-05-05 22:19:02 +0300

    PS-27 Added win and loss screens

--- game.py
+++ game.py
@@ -9,20 +9,21 @@ from Timer_Class import *
 
 PERS_COUNT = 3
 
+
 class Game():
     def __init__(self):
         pygame.mixer.pre_init(44100, -16, 2)
         pygame.init()
-        self.running, self.playing = True, False # цикл
+        self.running, self.playing = True, False  # цикл
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
-        self.DISPLAY_W, self.DISPLAY_H = 1280, 720 #разрешение экрана
-        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
-        self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
+        self.DISPLAY_W, self.DISPLAY_H = 1280, 720  # разрешение экрана
+        self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
+        self.window = pygame.display.set_mode(((self.DISPLAY_W, self.DISPLAY_H)))
         self.font_name = pygame.font.get_default_font()
         self.BLACK, self.WHITE = (255, 192, 200), (255, 255, 255)
         self.main_menu = MainMenu(self)
         self.levels = LevelsMenu(self)
-        self.lvl_length = 60 # time, seconds
+        self.lvl_length = 10  # time, seconds
         self.exit = ExitMenu(self)
         self.curr_menu = self.main_menu
         self.icon = pygame.image.load('img/screen4/persons/boy/little.png')
@@ -56,7 +57,7 @@ class Game():
                         if pers.rect.collidepoint(event.pos):
                             print("НАЖАЛИ НА ЧЕЛА")
                             moving = True
-                            moving_pers = pers # заполмнили, какого персонажа двигаем
+                            moving_pers = pers  # заполмнили, какого персонажа двигаем
                             break
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_BACKSPACE:
@@ -65,7 +66,7 @@ class Game():
                         self.start = time.time()
                         self.pause()
                         self.end = time.time()
-                        timer.time_beg += (self.end-self.start)
+                        timer.time_beg += (self.end - self.start)
                 if (moving == True):
                     mouse_position = pygame.mouse.get_pos()
                     moving_pers.set_coordinats(mouse_position[0], mouse_position[1])
@@ -77,7 +78,7 @@ class Game():
                         moving_pers.rect.x = 125
                     elif (moving_pers.rect.x > 1130):
                         moving_pers.rect.x = 1100
-                    if (moving_pers.rect.y < 408 and moving_pers.rect.y >150):
+                    if (moving_pers.rect.y < 408 and moving_pers.rect.y > 150):
                         moving_pers.rect.y = 408
                     elif (moving_pers.rect.y > 408):
                         moving_pers.rect.y = 408
@@ -85,21 +86,57 @@ class Game():
                         moving_pers.rect.y = 150
 
             self.check_events()
-##            if self.BACK_KEY:  # START_KEY:
-##                self.playing = False
-##            if self.START_KEY:
-##                self.start = time.time()
-##                print('вы на паузе')
-##                self.pause()
-##                self.end = time.time()
-##                timer.time_beg += (self.end-self.start)
-##                print('вернулись')
             self.display = pygame.image.load('img/screen4/house/home_bg_white_frame.png')
-            #timer            
+            # end of the game
             if timer.time_is_over(self):
-                break
+                count_persons = len(persons)
+                all_dance = 0
+                all_thirst = 0
+                all_hunger = 0
+                all_social = 0
+                for pers in persons:
+                    all_social += pers.get_social()
+                    all_dance  += pers.get_dance()
+                    all_hunger += pers.get_hunger()
+                    all_thirst += pers.get_thirst()
+                result_needs = (all_hunger + all_dance + all_social + all_thirst) / (4*count_persons)
+                print(result_needs)
+                if result_needs > 50: # выигрыш
+                    self.end_image = pygame.image.load('img/screen5/good/text.png')
+                    self.gift_image = pygame.image.load('img/screen5/good/diamond.png')
+                    self.gift_image = pygame.transform.scale(self.gift_image, (333, 222))
+                else:
+                    self.end_image = pygame.image.load('img/screen5/ugh/text.png')
+                    self.gift_image = pygame.image.load('img/screen5/ugh/nothing.png')
 
-            self.window.blit(self.display, (0,0))
+                self.paused = True
+                while self.paused:
+                    self.rect1 = self.end_image.get_rect()
+                    self.rect1.center = (640, 260)
+
+                    self.rect2 = self.gift_image.get_rect()
+                    self.rect2.center = (640, 480)
+
+                    self.running, self.playing = False, False
+
+                    for event in pygame.event.get():
+                        if event.type == pygame.KEYDOWN:
+                            print("нажали на выход из окна завершения игры")
+                            self.paused = False
+                            self.curr_menu.run_display = True
+                            g = Game()
+
+                            while g.running:
+                                g.curr_menu.display_menu()
+                                g.game_loop()
+                            break
+                    self.check_events()
+
+                    self.window.blit(self.end_image, self.rect1)
+                    self.window.blit(self.gift_image, self.rect2)
+                    pygame.display.update()
+
+            self.window.blit(self.display, (0, 0))
             self.window.blit(divan.image, divan.rect)
             self.window.blit(voda.image, voda.rect)
             self.window.blit(dancefloor.image, dancefloor.rect)
@@ -107,10 +144,10 @@ class Game():
             self.window.blit(timer.image, timer.rect)
             if (moving_pers):
                 moving_pers.show_needs(self)
-            
+
             for i in range(PERS_COUNT):
                 self.window.blit(person_group[i].image, person_group[i].rect)
-            #pygame.display.update() #обновляем дисплей
+            # pygame.display.update() #обновляем дисплей
 
             hit_list = pygame.sprite.spritecollide(voda, persons, False)
             if (hit_list):
@@ -127,7 +164,7 @@ class Game():
             for pers in persons:
                 if pers not in hit_list:
                     pers.dec_social()
-                    
+
             hit_list = pygame.sprite.spritecollide(dancefloor, persons, False)
             if (hit_list):
                 for pers in hit_list:
@@ -168,7 +205,7 @@ class Game():
 
     def check_events(self):
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: #конец игры
+            if event.type == pygame.QUIT:  # конец игры
                 self.running, self.playing = False, False
                 self.curr_menu.run_display = False
             if event.type == pygame.KEYDOWN:
@@ -184,9 +221,9 @@ class Game():
     def reset_keys(self):
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
 
-    def draw_text(self, text, size, x, y ):
-        font = pygame.font.Font(self.font_name,size)
+    def draw_text(self, text, size, x, y):
+        font = pygame.font.Font(self.font_name, size)
         text_surface = font.render(text, True, self.WHITE)
         text_rect = text_surface.get_rect()
-        text_rect.center = (x,y)
-        self.display.blit(text_surface,text_rect)
+        text_rect.center = (x, y)
+        self.display.blit(text_surface, text_rect)
